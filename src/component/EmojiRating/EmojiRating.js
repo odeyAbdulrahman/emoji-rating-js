6e9357de0ba9e8f3d279c40cc30c67e2d0ce9dbb
Odey Abdalrahman <odey.aroo@gmail.com>
2022-02-21 11:02:43 +0400

    upate style file

--- src/component/EmojiRating/EmojiRating.js
+++ src/component/EmojiRating/EmojiRating.js
@@ -1,37 +1,44 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, {useState} from "react";
 import "./emojiRating.css";
-
 function EmojiRating({
   data,
   onChange,
   style
 }) {
   const [selected, setSelected] = useState(0);
-  return (
-    <>
-      <div className="emoji-box">
-        {data &&
-          data.map((item) => (
-            <div
-              key={item.id}
-              className="emoji-sub-box"
-              onClick={() => {
-                onChange(item);
-                setSelected(item.id);
-              }}
-            >
-              <img
-                className="emoji-img"
-                src={item.img}
-                style={selected === item.id ? style.img : null}
-              />
-              <label style={selected === item.id ? style.text : null}>{item.name}</label>
-            </div>
-          ))}
-      </div>
-    </>
-  );
+  try{
+    return (
+      <>
+        <div className="emoji-box">
+          {data &&
+            data.map((item) => (
+              <div
+                key={item.id}
+                className="emoji-sub-box"
+                onClick={() => {
+                  onChange(item);
+                  setSelected(item.id);
+                }}
+              >
+                <img
+                  className="emoji-img"
+                  src={item.img}
+                  style={selected === item.id ? style.img : null}
+                />
+                <label style={selected === item.id ? style.text : null}>{item.name}</label>
+              </div>
+            ))}
+        </div>
+      </>
+    );
+  } catch(error){
+    console.info('error: ( ' + error + ' )')
+    return(
+      <>
+      <h3 className="error-msg">😧 I did something wrong 😧</h3>
+      </>
+    );
+  }
 }
-
 export default EmojiRating;
--- src/stories/EmojesStoryBook.stories.js
+++ src/stories/EmojesStoryBook.stories.js
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
 import { EmojiRating, Emojes, Styles } from "../../dist/index.es";
+import { ErrorBoundary } from "react-error-boundary";
 
 const stories = storiesOf("emojes app", module);
 stories.add("App", () => {
   const [Emoji, setEmoji] = useState({});
   const getEmoji = (item) => setEmoji(item);
   return (
-    <>
+    <ErrorBoundary>
       <br />
-      <EmojiRating
-        data={Emojes}
-        onChange={getEmoji}
-        style={Styles.style2}
-      />
+      <EmojiRating data={Emojes} onChange={getEmoji} style={Styles.blueStyle2} />
+      <br />
       <h4>your rating is: {Emoji.name}</h4>
-    </>
+    </ErrorBoundary>
   );
 });
